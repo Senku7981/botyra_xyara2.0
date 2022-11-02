f7cfc0a6f2f8a1f0670334baa3f1a10f4f1c7abd
chelovek12565 <bas180407@mail.tu>
2022-11-02 21:46:53 +0500

    qwerty1234

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 from aiogram import executor, bot
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from db import db_session
-
+from data import config
 from database import work_db
 from loader import dp
+import logging
+import asyncio
+
 
 
 async def on_startup(dp):
@@ -13,6 +18,23 @@ async def on_startup(dp):
     await work_db.create_tables()
     # await db_session.global_init('database/database.sqlite')
 
+
+async def main():
+    """initialize and run bot"""
+    bot = Bot(config.TOKEN, parse_mode=types.ParseMode.HTML)
+    storage = MemoryStorage()
+    dp = Dispatcher(bot, storage=storage)
+
+    await on_startup(dp)
+    await dp.start_polling()
+    # finally:
+    #     await on_shutdown(dp)
+    #     await bot.session.close()
+
+# if __name__ == '__main__':
+#     # db_session.global_init('database/database.sqlite')
+#     executor.start_polling(dp, on_startup=on_startup)
 if __name__ == '__main__':
-    # db_session.global_init('database/database.sqlite')
-    executor.start_polling(dp, on_startup=on_startup(dp))
+    asyncio.run(main())
+    # except (KeyboardInterrupt, SystemExit):
+    #     logging.info("Bye!")
--- loader.py
+++ loader.py
@@ -7,6 +7,7 @@ from db import db_session
 
 # db_session.global_init('database/database.sqlite')
 
+
 bot = Bot(config.TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
